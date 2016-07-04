2bddbb83adf208f28b1f399a04e1d9ce66910977
Heather Conley <hconley6695@gmail.com>
2016-07-04 15:10:44 -0400

    added current time component

--- src/js/current_time.js
+++ src/js/current_time.js
@@ -1,1 +1,15 @@
 import React, {Component} from 'react';
+
+export default class CurrentTime extends Component {
+
+
+	render () {
+		return (
+			<div className="current-time-div">
+				
+
+			</div>
+		);
+	}
+
+}
